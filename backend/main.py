53175a3c246496e874ea149112c60c984410b79d
tahirsaeedtech-stack <tahirsaeed.tech@gmail.com>
2026-08-27 12:11:45 +0500

    Complete working Telco churn application

--- backend/main.py
+++ backend/main.py
@@ -17,10 +17,12 @@ model = joblib.load(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
