c1a430966bd7137c24f008b9b9cdcedd9146a24c
tahirsaeedtech-stack <tahirsaeed.tech@gmail.com>
2026-09-03 10:30:19 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,17 +31,6 @@ model = joblib.load(
 )
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
-
 @app.get("/")
 def root():
     return {
